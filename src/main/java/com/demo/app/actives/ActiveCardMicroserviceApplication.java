755b20d8cdda268d330a4541273d8584512b7eb6
Dennilson190414 <103059691+Dennilson190414@users.noreply.github.com>
2022-04-13 17:34:13 -0500

    Template active-card

---
+++ src/main/java/com/demo/app/actives/ActiveCardMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.app.actives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveCardMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveCardMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/app/actives/ActiveCardMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.app.actives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveCardMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
